4af7b5867c02d575b9a32c8a379da3ec6c651a25
indre101 <zygaityte.indre@gmail.com>
2020-02-04 14:30:10 +0100

    appends templates

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const studentTemplate = document.querySelector(".studentTemplate").textContent
+const studentTemplate = document.querySelector(".studentTemplate").content;
 const students = document.querySelector(".students");
 
-window.addEventListener("load", init)
+window.addEventListener("load", init);
 
 function init() {
-  fetch("students1991.json").then(res => res.json()).then(data => {
-    data.forEach(student => {
-      createStudentCards(student)
+  fetch("students1991.json")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(student => {
+        createStudentCards(student);
+      });
     });
-  })
 }
 
 function createStudentCards(student) {
-  const clnStudent = studentTemplate.clone
+  const clnStudent = studentTemplate.cloneNode(true);
 
   students.appendChild(clnStudent);
-
-
 }
